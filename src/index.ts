6f3f8475872578d9fb04aa98a19340b2265e2889
tetunori <tetunori@gmail.com>
2023-05-21 15:17:07 +0900

    alpha test

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@ import Runtime from 'scratch-vm/src/engine/runtime'
 import ArgumentType from 'scratch-vm/src/extension-support/argument-type'
 import BlockType from 'scratch-vm/src/extension-support/block-type'
 import Cast from 'scratch-vm/src/util/cast'
-import Peer from 'skyway-js';
-
+import Peer from 'skyway-js'
 
 class TextChatSkyWayExtension {
   private runtime: Runtime
-
-  private gamepadIndex: number
 
   private static apikey: string = ''
   private isReceived: boolean = false
@@ -19,27 +16,32 @@ class TextChatSkyWayExtension {
 
   constructor(runtime: Runtime) {
     this.runtime = runtime
+    this.inputApiKey()
+  }
 
+  inputApiKey() {
     if (TextChatSkyWayExtension.apikey === '') {
       const apikey = window.prompt('SkyWayのAPIキーを入力してください: ', '')
-      if (apikey) {
+
+      if (apikey !== '' && apikey !== null) {
         TextChatSkyWayExtension.apikey = apikey
         console.log(TextChatSkyWayExtension.apikey)
-        this.peer = new Peer({key: apikey})
-
+        this.peer = new Peer({ key: apikey })
       }
     }
   }
 
   getInfo() {
+    const menuIcon =
+      'data:image/svg+xml;charset=utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20overflow%3D%22hidden%22%3E%3Cpath%20d%3D%22M12%203C17.5%203%2022%206.58%2022%2011%2022%2015.42%2017.5%2019%2012%2019%2010.76%2019%209.57%2018.82%208.47%2018.5%205.55%2021%202%2021%202%2021%204.33%2018.67%204.7%2017.1%204.75%2016.5%203.05%2015.07%202%2013.13%202%2011%202%206.58%206.5%203%2012%203Z%22%20stroke%3D%22%23595959%22%20fill%3D%22%23FFFFFF%22%2F%3E%3C%2Fsvg%3E'
     return {
       id: 'testchatskyway',
       name: 'チャット',
-      menuIconURI: 'assets/images/menuIcon.png',
-      blockIconURI: 'assets/images/menuIcon.png',
-      color1: '#e9c46a',
-      color2: '#f4a261',
-      color3: '#e76f51',
+      menuIconURI: menuIcon,
+      blockIconURI: menuIcon,
+      color1: '#8b5e34',
+      color2: '#6f4518',
+      color3: '#603808',
 
       blocks: [
         {
@@ -68,19 +70,18 @@ class TextChatSkyWayExtension {
         {
           opcode: 'leaveRoom',
           blockType: BlockType.COMMAND,
-          text: '退室する',
+          text: '退室する'
         },
         {
           opcode: 'whenChatTextReceived',
           blockType: BlockType.HAT,
-          text: 'チャットテキストを受け取ったとき',
+          text: 'チャットテキストを受け取ったとき'
         },
         {
           opcode: 'getReceivedChatText',
           blockType: BlockType.REPORTER,
-          text: '受け取ったテキスト',
-        },
-
+          text: '受け取ったテキスト'
+        }
       ],
 
       menus: {
@@ -104,60 +105,62 @@ class TextChatSkyWayExtension {
   joinRoom(args) {
     console.log('joinRoom: ' + args.ROOM)
 
-    if(!this.room){
-      this.room = this.peer.joinRoom(args.ROOM, {mode: 'p2p'});
-      console.log(args.ROOM + 'に入室しました');
-
-      this.room.on('data', function(data){
-        console.log('received:' + data.data);
-        this.receivedChatText = data.data
-        this.isReceived = true;
-      }.bind(this));
-
-    }else{
-      console.log('重複して入室はできません。一度退出してください。');
+    // ignored if api key was alread input.
+    this.inputApiKey()
+    if (!this.peer) {
+      return
     }
 
+    if (!this.room) {
+      this.room = this.peer.joinRoom(args.ROOM, { mode: 'p2p' })
+      console.log(args.ROOM + 'に入室しました')
+
+      this.room.on(
+        'data',
+        function (data) {
+          console.log('received:' + data.data)
+          this.receivedChatText = data.data
+          this.isReceived = true
+        }.bind(this)
+      )
+    } else {
+      console.log('重複して入室はできません。一度退出してください。')
+    }
   }
 
   sendChatText(args) {
     // console.log('sendChatText: ' + args.TEXT)
 
-    if(this.room){
-      this.room.send(args.TEXT);
+    if (this.room) {
+      this.room.send(args.TEXT)
     }
   }
 
   leaveRoom() {
     // console.log('leaveRoom')
-    if(this.room){
-      this.room.close();
+    if (this.room) {
+      this.room.close()
       console.log('退室しました')
-      this.room = undefined;
+      this.room = undefined
     }
-
   }
 
   whenChatTextReceived(args) {
     // console.log('whenChatTextReceived')
 
-    if(this.isReceived){
+    if (this.isReceived) {
       this.isReceived = false
       return true
     }
 
-    return false;
-
+    return false
   }
 
   getReceivedChatText(args) {
     // console.log('getReceivedChatText')
 
-    return this.receivedChatText;
+    return this.receivedChatText
   }
-
-
-
 }
 
 export default TextChatSkyWayExtension
