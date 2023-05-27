57965aaf65e33da4af162b10dbf9bf59f3fde48e
tetunori <tetunori@gmail.com>
2023-05-27 19:27:11 +0900

    Add api key block to Esheepicques

--- src/Esheepicques.ts
+++ src/Esheepicques.ts
@@ -1,13 +1,37 @@
 import TextChatSkyWayExtensionCore from './core'
-
+import ArgumentType from 'scratch-vm/src/extension-support/argument-type'
+import BlockType from 'scratch-vm/src/extension-support/block-type'
 ;(function (Scratch) {
   'use strict'
 
   class TextChatSkyWayExtensionES extends TextChatSkyWayExtensionCore {
+    private static _apikey: string = ''
+
     getApiKey() {
-      return ''
+      return TextChatSkyWayExtensionES._apikey
+    }
+
+    getInfo() {
+      const retObj = super.getInfo()
+      retObj.blocks.unshift({
+        opcode: 'registApiKey',
+        blockType: BlockType.COMMAND,
+        text: 'APIキー [APIKEY] を登録する',
+        arguments: {
+          APIKEY: {
+            type: ArgumentType.STRING,
+            defaultValue: 'APIキー'
+          }
+        }
+      })
+      return retObj
+    }
+
+    registApiKey(args) {
+      // console.log('registApiKey: ' + args.APIKEY)
+      TextChatSkyWayExtensionES._apikey = args.APIKEY
+      this.inputApiKey();
     }
   }
-
   Scratch.extensions.register(new TextChatSkyWayExtensionES())
 })(Scratch)
